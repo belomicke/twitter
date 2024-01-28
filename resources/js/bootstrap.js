d140c6a2f8ad8d439eac1d49a75aff823e8a0514
Mickael Belous <rellmanwinner@gmail.com>
2024-01-28 18:10:25 +0300

    feature: csrf and bearer token on each request

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,23 @@
-import axios from 'axios';
-window.axios = axios;
+import axios from 'axios'
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios = axios
+
+const token = localStorage.getItem('token')
+
+let csrf = document.head.querySelector('meta[name="csrf-token"]')
+
+if (csrf) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = csrf.content
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token')
+}
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+window.axios.defaults.headers.common['Accept'] = 'application/json'
+
+if (token) {
+    window.axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
